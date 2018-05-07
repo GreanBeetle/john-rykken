4ad4fbcc9f888f80a4028d08641bf8a512b3cb55
John Rykken <john.rykken@gmail.com>
2018-05-07 15:34:15 -0700

    Install underwood font.

--- db/schema.rb
+++ db/schema.rb
@@ -29,12 +29,6 @@ ActiveRecord::Schema.define(version: 2018_04_27_211607) do
     t.string "explanation"
   end
 
-  create_table "terms_tables", force: :cascade do |t|
-    t.string "name"
-    t.string "blurb"
-    t.string "explanation"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
