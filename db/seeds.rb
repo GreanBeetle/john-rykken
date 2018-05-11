782fd0720ef98d81bd9c3f4a04538d8a6efa777e
John Rykken <john.rykken@gmail.com>
2018-05-11 16:30:31 -0700

    Revert active admin debacle. Successfully seperate user and adminuser login, signup.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@ Term.destroy_all
 end
 
 p "Created #{Term.count} terms."
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
