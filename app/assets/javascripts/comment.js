8b07adc63eecf140cd01e29666106a797b0908ea
Student <example@example.com>
2018-05-14 09:42:32 -0700

    Start adding AJAX for comments.

---
+++ app/assets/javascripts/comment.js
@@ -0,0 +1,5 @@
+$(function(){
+  $('#new_comment').click(function(){
+    alert('nice. take a deep breath. commit. keep going.')
+  });
+});
---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+def CommentsController < ApplicationController
+
+  def new
+    @term = Term.find(params[:term_id])
+    @comment = @term.comments.new
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
+
+end
