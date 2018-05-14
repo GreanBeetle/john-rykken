a6f3ee942fdfcc517d3d46821149b86b0207773f
Student <example@example.com>
2018-05-14 10:25:17 -0700

    Implement AJAX 'create comment', append to DOM.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,9 +5,43 @@ class CommentsController < ApplicationController
     @comment = @term.comments.new
   end
 
+  def create
+    @term = Term.find(params[:term_id])
+    @comment = @term.comments.new(comment_params)
+    # @comment.user_id = current_user.id
+    @comment.save
+    respond_to do |format|
+      format.html { redirect_to term_path(@term) }
+      format.js
+    end
+
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:body)
     end
 
 end
+
+# REFERENCE CODE
+# def create
+#     @book = Book.new(book_params)
+#     @book.available = true
+#     @book.save
+#     respond_to do |format|
+#       format.html { redirect_to books_path }
+#       format.js
+#     end
+#   end
+#
+#   def create
+#     @animal = Animal.find(params[:animal_id])
+#     @sighting = @animal.sightings.new(sighting_params)
+#     @animal.update(sighted: true)
+#     if @sighting.save
+#       redirect_to animal_path(@sighting.animal)
+#     else
+#       render :new
+#     end
+#   end
