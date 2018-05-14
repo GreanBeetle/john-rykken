3e0a3b525fa891e3596057d792bc84724250d7b6
Student <example@example.com>
2018-05-14 11:25:48 -0700

    Revise error message for new comment. Add login, signup, back links.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
     @current_user = current_user
     @term = Term.find(params[:term_id])
     @comment = @term.comments.new
-    
+
   end
 
   def create
@@ -25,36 +25,3 @@ class CommentsController < ApplicationController
     end
 
 end
-
-
-def new
-    if current_user
-      @term = Term.find(params[:term_id])
-      @comment = @term.comments.new
-    else
-      respond_to do |format|
-        format.html { redirect_to  }
-      end
-    end
-  end
-# REFERENCE CODE
-# def create
-#     @book = Book.new(book_params)
-#     @book.available = true
-#     @book.save
-#     respond_to do |format|
-#       format.html { redirect_to books_path }
-#       format.js
-#     end
-#   end
-#
-#   def create
-#     @animal = Animal.find(params[:animal_id])
-#     @sighting = @animal.sightings.new(sighting_params)
-#     @animal.update(sighted: true)
-#     if @sighting.save
-#       redirect_to animal_path(@sighting.animal)
-#     else
-#       render :new
-#     end
-#   end
